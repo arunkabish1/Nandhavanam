b1c8d763cbead23609281535163ee7367292bc03
Arun <720721110060@hicet.ac.in>
2025-11-07 14:30:03 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -281,8 +281,8 @@ app.put("/contacts/:id" ,async(req,res)=>{
 
 })
 
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000, () =>
-  console.log("🚀 Server running on http://localhost:5000")
-);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
